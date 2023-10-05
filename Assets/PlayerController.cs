ce27e29dc6cfe049d1e4ab0ac4a247b46231487c
Enething <enethingofficial@gmail.com>
2023-10-05 03:16:48 +0200

    Reworked Weapon Sway

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,14 +11,12 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Controller = GetComponent<CharacterController>();
-        InitializeHeadBob();
     }
     void Update()
     {
         Cursor.lockState = CursorLockMode.Locked;
         PlayerMovemement();
         PlayerRotation();
-        HeadBob();
     }
 
     #region Movement
@@ -29,10 +27,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float JumpStrength;
     [SerializeField] private float WalkSpeed;
     [SerializeField] private float RunSpeed;
+    [SerializeField] public bool isGrounded;
     private CharacterController Controller;
-    private Vector3 CurrentForceVelocity;
-    private Vector3 CurrentMoveVelocity;
-    private Vector3 MoveDampVelocity;
+    [HideInInspector] public Vector3 CurrentForceVelocity;
+    [HideInInspector] public Vector3 CurrentMoveVelocity;
+    [HideInInspector] public Vector3 MoveDampVelocity;
+
     private void PlayerMovemement()
     {
         Vector3 PlayerInput = new Vector3
@@ -62,6 +62,7 @@ public class PlayerController : MonoBehaviour
         Ray groundCheckRay = new Ray(transform.position, Vector3.down);
         if (Physics.Raycast(groundCheckRay, 1.1f))
         {
+            isGrounded = true;
             CurrentForceVelocity.y = -2f;
 
             if (Input.GetKey(KeyCode.Space))
@@ -71,6 +72,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
+            isGrounded = false;
             CurrentForceVelocity.y -= GravityStrength * Time.deltaTime;
         }
 
@@ -103,54 +105,6 @@ public class PlayerController : MonoBehaviour
         transform.eulerAngles = new Vector3(0f, Rotation.y, 0f);
         PlayerCamera.localEulerAngles = new Vector3(Rotation.x, 0f, 0f);
     }
-
-    #endregion
-
-    #region Head-Bobbing
-    [Header("HEAD-BOBBING")]
-    [SerializeField] private bool EnableHeadBobbing = true;
-    [SerializeField, Range(0f, 0.1f)] private float HeadBobAmplitude = 0.015f;
-    [SerializeField, Range(0f, 30f)] private float HeadBobFrequency = 10f;
-    [SerializeField] private Camera HeadBobCamera;
-    [SerializeField] private Transform HeadBobHolder;
-
-    private float HeadBobToggleSpeed = 3.0f;
-    private Vector3 HeadBobStartPos;
-    private void InitializeHeadBob()
-    {
-        HeadBobStartPos = HeadBobCamera.transform.localPosition;
-    }
-    private void ResetHeadBobPosition()
-    {
-        if (HeadBobCamera.transform.localPosition == HeadBobStartPos) return;
-        HeadBobCamera.transform.localPosition = Vector3.Lerp(HeadBobCamera.transform.localPosition, HeadBobStartPos, 1 * Time.deltaTime);
-    }
-    private void HeadBob()
-    {
-        if (!EnableHeadBobbing)
-            return;
-
-        /* EnableCheck */
-        float speed = new Vector3(CurrentMoveVelocity.x, 0, CurrentMoveVelocity.z).magnitude;
-        if (speed < HeadBobToggleSpeed | !Controller.isGrounded) 
-            return;
-
-        /* Head Up-Down-Left-Right Movement */
-        Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Sin(Time.time * HeadBobFrequency) * HeadBobAmplitude * 2.5f;
-        pos.x += Mathf.Cos(Time.time * HeadBobFrequency / 2) * HeadBobAmplitude * 2;
-        HeadBobCamera.transform.localPosition += pos;
-
-        /* Go back to normal Position */
-        ResetHeadBobPosition();
-
-        /* Look at target point */
-        //vector3 targetpoint = new vector3(transform.position.x, transform.position.x + headbobholder.localposition.y, transform.position.z);
-        //targetpoint += headbobholder.forward * 15f;
-        //headbobcamera.transform.lookat(targetpoint);
-    }
-
-
 
     #endregion
 }
